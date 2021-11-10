32221da579407a2a87f5bc98c9100a231dd0d251
Richard Dubný <72037908+R1xhark@users.noreply.github.com>
2021-11-10 22:57:05 +0100

    Create Uvod.java
    Uvod do programu, pouze pozdrav.

---
+++ Uvod.java
@@ -0,0 +1,10 @@
+package koronaukol;
+
+public class Uvod {
+    public void pozdrav(){
+        System.out.println("Vítejte v testování ministerstva zdravotnictví proti CoVid19:");
+        
+    }
+            
+    
+}
