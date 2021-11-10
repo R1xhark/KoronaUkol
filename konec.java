3d9808a9aa92fd5c32dea874634316644f9fc6c8
Richard Dubný <72037908+R1xhark@users.noreply.github.com>
2021-11-10 23:08:30 +0100

    KONTAKT A CREDITS
    Konec, pouze credits a rozlouceni, kontakt atd.

---
+++ konec.java
@@ -0,0 +1,15 @@
+package koronaukol;
+
+public class Konec {
+   
+    public void getInfo(){
+        int rocnik=4;
+        String TridaA=".A ";
+        String Email="richard@dubny.cz";
+        System.out.println("Děkujeme za využití našeho testovacího Java programu.");
+        System.out.println("Vytvořil Richard Dubný "+rocnik+TridaA+Email);
+        System.out.println("www.dubny.cz");
+        
+    }
+    
+}
